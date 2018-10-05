7e7f8802dc88c0b835025f4d53c190e10a290dfd
lamborot-eliot <lamboroteliot@gmail.com>
2018-10-05 13:34:11 +0200

    ajout date activ

--- dataminer.py
+++ dataminer.py
@@ -7,12 +7,19 @@ import requests
 import html
 import json
 import getpass
+import urllib
+import re
+import datetime
+
 
 URL_LOGIN = 'https://espace-client-connexion.enedis.fr'
 URL_API_BASE = 'https://espace-client-particuliers.enedis.fr/group/espace-particuliers'
+URL_DATE_ACTIV = '/courbe-de-charge'
 URL_API_LOGIN = '/auth/UI/Login'
 URL_API_HOME = '/home'
 URL_API_DATA = '/suivi-de-consommation'
+
+date_activ = ""
 
 
 #liste des choix(= resource_id) a envoyer dans la requete
@@ -23,21 +30,40 @@ an='urlCdcAn'
 
 def login(username, password):
 
-    session = requests.Session()
+    #session = requests.Session()
+    with requests.Session() as session:
 
-    #contenue de la requete (voir inspecteur navigateur)
-    # (https://deadliestwebattacks.com/2010/04/12/login-forms/)
-    payload = {'IDToken1': username,
-               'IDToken2': password,
-               'SunQueryParamsString': base64.b64encode(b'realm=particuliers'),#obligatoire dans la requete (je sais pas trop pourquoi sous cette forme)
-               'encoded': 'true',#facultatif mais sympa a mettre
-               'gx_charset': 'UTF-8'#pareil
-               }
+        #contenue de la requete (voir inspecteur navigateur)
+        # (https://deadliestwebattacks.com/2010/04/12/login-forms/)
+        payload = {'IDToken1': username,
+                   'IDToken2': password,
+                   'SunQueryParamsString': base64.b64encode(b'realm=particuliers'),#obligatoire dans la requete (je sais pas trop pourquoi sous cette forme)
+                   'encoded': 'true',#facultatif mais sympa a mettre
+                   'gx_charset': 'UTF-8'#pareil
+                   }
 
-    #on envoie les données
-    requete = session.post(URL_LOGIN + URL_API_LOGIN, data=payload, allow_redirects=False)
+        #on envoie les données
 
-    return session
+        requete = session.post(URL_LOGIN + URL_API_LOGIN, data=payload, allow_redirects=False)
+
+        s = session.get(URL_API_BASE + URL_DATE_ACTIV, allow_redirects = False)
+
+
+        if 302 == s.status_code:
+            #des fois on a un status 302 (je sais pas pourquoi) donc on renvoie la requete et ca marche
+            s = session.get(URL_API_BASE + URL_DATE_ACTIV, allow_redirects = False)
+
+
+        pattern = re.compile(r'\d\d/\d\d/\d\d\d\d')
+        matches = pattern.findall(s.text)
+
+        res = matches[0]
+        slice = res[0:9]
+        temp = str(int(res[9])-1)
+        date_activ = slice + temp
+
+
+        return session
 ###################################################
 
 
@@ -61,33 +87,38 @@ def recup_donnee(session, resource_id, debut=None, fin=None):
         'p_p_col_count': 2
     }
 
+    #requete du json
     requete = session.post(URL_API_BASE + URL_API_DATA, allow_redirects=False, data=formData, params=queryStringParameters)
+
 
     if 302 == requete.status_code:
         #des fois on a un status 302 (je sais pas pourquoi) donc on renvoie la requete et ca marche
         requete = session.post(URL_API_BASE + URL_API_DATA, allow_redirects=False, data=formData, params=queryStringParameters)
 
-
-
     res = requete.json()
 
 
 
-    return res
 ###################################################
-
 
 
 
 
 ########### MAIN ###########
 
-user=input("veuillez entrer votre mail : ")
-pwd = getpass.getpass(prompt='veuillez entrer votre mdp : ')
+#user=input("veuillez entrer votre mail : ")
+#pwd = getpass.getpass(prompt='veuillez entrer votre mdp : ')
+now = ""
+now += str(datetime.datetime.now().day)
+now += "/"
+now += str(datetime.datetime.now().month)
+now += "/"
+now += str(datetime.datetime.now().year)
 
+print (now)
 
-monlog=login(user,pwd)
-doto=recup_donnee(monlog,jour,"11/09/2018","13/09/2018")
+monlog=login("catounono@aol.com","Elioteliot@69")
+doto=recup_donnee(monlog,jour,date_activ,now)
 
 print (json.dumps(doto, indent=4, sort_keys=True))
 

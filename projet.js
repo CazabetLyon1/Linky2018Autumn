7a8694d3c63f3ef6d4deab7cf6a273b15bdaaa64
vigier-lucas <43638259+vigier-lucas@users.noreply.github.com>
2018-10-14 18:31:08 +0200

    Ajout modal, et maj des boutons

--- projet.js
+++ projet.js
@@ -2,8 +2,14 @@ $( document ).ready(function() {
 
   // Open navbarSide when button is clicked
   $('#bars').on('click', function() {
-    if($('#menu').hasClass('reveal')) $('#menu').removeClass('reveal');
-    else  $('#menu').addClass('reveal');
+    if($('#menu').hasClass('reveal')) {
+      $('#menu').removeClass('reveal');
+      $('#bars').removeClass('fa-spin');
+    }
+    else{ 
+     $('#menu').addClass('reveal');
+     $('#bars').addClass('fa-spin');
+   }
   });
 
 

549d6c6859f28c11e96b1330b8545c9ad9152b54
SCOUFLAIRE THOMAS p1609419 <p1609419@B710L0905.univ-lyon1.fr>
2018-11-30 15:57:42 +0100

    machinelearning

--- machine_learnig.py
+++ machine_learnig.py
@@ -35,3 +35,51 @@ print(H.size)
 print(W.size)
 np.savetxt("W.csv", W, delimiter=";")
 np.savetxt("H.csv", H, delimiter=";")
+
+
+donnees_patern_1 = [];
+donnees_patern_2 = [];
+donnees_patern_3 = [];
+donnees_patern_4 = [];
+
+prix_moyen_kWh = 0.15;
+def donner_patern(numero_patern, H, W, prix_moyen_kWh){
+	donnee_patern = [];
+	semaine = 0;
+	jour = 1;
+	somme = 0;
+	for(i = 0; i<H.shape[]; i++){
+		if(jour == 7){
+			somme = somme + H[numero_patern][i];
+			donnees_patern.append(somme);
+			somme = 0;
+			jour = 1;
+		}
+		else
+		somme = somme + H[0][i];
+		jour++;
+	}
+	donnees_patern.append(somme);
+	
+	for(i = 0;i<len(donnee_patern) ;i++){
+		donnee_patern[i] = W[i][numero_patern]*donnee_patern[i]*prix_moyen_kWh;
+	}
+	return donnee_patern;
+}
+
+
+donnees_patern_1 = donner_patern(0, H, W, prix_moyen_kWh);
+donnees_patern_2 = donner_patern(0, H, W, prix_moyen_kWh);
+donnees_patern_3 = donner_patern(0, H, W, prix_moyen_kWh);
+donnees_patern_4 = donner_patern(0, H, W, prix_moyen_kWh);
+
+
+
+
+
+
+
+
+
+
+

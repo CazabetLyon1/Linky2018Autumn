3e3f0c3929bd489c4f5f32d787a72e317e4c9202
lamborot-eliot <lamboroteliot@gmail.com>
2018-12-06 16:02:42 +0100

    fin graph dynamique

--- machine_learnig.py
+++ machine_learnig.py
@@ -2,6 +2,8 @@ from dataminer import *
 import numpy as np
 from sklearn.decomposition import NMF
 
+
+
 print(len(trans))
 ligne = []
 matrice = []
@@ -37,49 +39,39 @@ np.savetxt("W.csv", W, delimiter=";")
 np.savetxt("H.csv", H, delimiter=";")
 
 
-donnees_patern_1 = [];
-donnees_patern_2 = [];
-donnees_patern_3 = [];
-donnees_patern_4 = [];
+donnees_patern_1 = []
+donnees_patern_2 = []
+donnees_patern_3 = []
+donnees_patern_4 = []
 
-prix_moyen_kWh = 0.15;
-def donner_patern(numero_patern, H, W, prix_moyen_kWh){
-	donnee_patern = [];
-	semaine = 0;
-	jour = 1;
-	somme = 0;
-	for(i = 0; i<H.shape[]; i++){
-		if(jour == 7){
-			somme = somme + H[numero_patern][i];
-			donnees_patern.append(somme);
-			somme = 0;
-			jour = 1;
-		}
-		else
-		somme = somme + H[0][i];
-		jour++;
-	}
-	donnees_patern.append(somme);
-	
-	for(i = 0;i<len(donnee_patern) ;i++){
-		donnee_patern[i] = W[i][numero_patern]*donnee_patern[i]*prix_moyen_kWh;
-	}
-	return donnee_patern;
-}
+prix_moyen_kWh = 1
+
+def donner_patern(numero_patern, H, W, prix_moyen_kWh):
+    donnee_patern = []
+    semaine = 0
+    jour = 1
+    somme = 0
+    for i in range(0,H.shape[1]):
+        if(jour == 7):
+            somme = somme + H[numero_patern][i]
+            donnee_patern.append(somme)
+            somme = 0
+            jour = 1
+        else:
+            somme = somme + H[numero_patern][i]
+            jour += 1
+    donnee_patern.append(somme)
+
+    for i in range(0,len(donnee_patern)-1):
+        donnee_patern[i] = W[i][numero_patern]*donnee_patern[i]*prix_moyen_kWh
+    return donnee_patern
 
 
-donnees_patern_1 = donner_patern(0, H, W, prix_moyen_kWh);
-donnees_patern_2 = donner_patern(0, H, W, prix_moyen_kWh);
-donnees_patern_3 = donner_patern(0, H, W, prix_moyen_kWh);
-donnees_patern_4 = donner_patern(0, H, W, prix_moyen_kWh);
-
-
-
-
-
-
-
-
-
-
-
+donnees_patern_1 = donner_patern(0, H, W, prix_moyen_kWh)
+print(donnees_patern_1)
+donnees_patern_2 = donner_patern(1, H, W, prix_moyen_kWh)
+print(donnees_patern_2)
+donnees_patern_3 = donner_patern(2, H, W, prix_moyen_kWh)
+print(donnees_patern_3)
+donnees_patern_4 = donner_patern(3, H, W, prix_moyen_kWh)
+print(donnees_patern_4)

9cb648b072435f60014915541fc29044b2e3f5c2
vigier-lucas <43638259+vigier-lucas@users.noreply.github.com>
2018-12-17 21:58:35 +0100

    MAJ de l'ergonomie

--- server.js
+++ server.js
@@ -29,29 +29,16 @@ app.get('/', (req, res) => {
 app.post('/connection', (req, res) => {
 	var email = req.body.email;
 	var pwd = req.body.password;
-	console.log("Dbut requete");
 	exec('python3 dataminer.py '+email+' '+pwd, (error, stdout, stderr) => {
 		if (error) {
 			res.sendStatus(400);
 			return;
 		}
-		console.log("Fin requete");
 		fs.readFile(stdout.split('\n')[0]+'.json', 'utf8', function (err, data) {
 			if (err) throw err;
 			obj = JSON.parse(data);
 			res.json(data);
 		});
-	});
-});
-
-app.post('/clicked', (req, res) => {
-	exec('python3 dataminer.py catounono@aol.com Elioteliot@69', (error, stdout, stderr) => {
-		if (error) {
-			res.sendStatus(400);
-			return;
-		}
-		console.log(stdout.split('\n')[0]);
-		console.log('easy');
 	});
 });
 
